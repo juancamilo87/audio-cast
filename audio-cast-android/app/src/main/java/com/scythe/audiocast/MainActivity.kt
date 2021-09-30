69d27b370332e44fc197caabb2fc79199b66a887
juancamilo87 <juancamilo87@gmail.com>
2021-10-01 00:10:58 +0300

    Re create android App with Jetpack Compose
    Resolves #6

---
+++ audio-cast-android/app/src/main/java/com/scythe/audiocast/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.scythe.audiocast
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.scythe.audiocast.ui.theme.AudioCastTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AudioCastTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AudioCastTheme {
+        Greeting("Android")
+    }
+}
